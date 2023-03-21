e82ee067a02431def7ef5dc49ea585d781e4ded6
Louis Mantell <loiuismantell@Louis-iMac.local>
2023-03-20 20:07:11 -0400

    api

--- mern-stack-3/src/api.js
+++ mern-stack-3/src/api.js
@@ -1,7 +1,7 @@
 import bootcamp from "./components/bootcamp"
 import review from "./components/review"
 
-const BASE_URL =
+const BASE_URL = '';
 
 export async function getBootcamps() {
     const response = await fetch(`${BASE_URL}/bootcamp`);
