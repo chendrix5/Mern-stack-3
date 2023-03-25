5a1d35c5b3d1769269c7b46809c193ebfd541cc2
Klyrasel <KaiLyrasel@outlook.com>
2023-03-25 19:08:35 -0400

    bootstrap navbar

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,24 +1,32 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
 
 function Nav() {
 
        return (
-        <nav>
-            <ul>
-                <li>
-                    <a href="home.js">Home</a>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">BOOTCAMP REVIEWS</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                 </button>
+         <div class="collapse navbar-collapse" id="navbarSupportedContent">
+             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                       <a class="nav-link inactive" aria-current="page" href="#">HOME</a>
                 </li>
-                <li>
-                    <a href="Reviews.js">Reviews</a>
+                <li class="nav-item">
+                        <a class="nav-link inactive" aria-current="page" href="#">LEAVE A REVIEW</a>
                 </li>
-                <li>
-                    <a href="Reviews.js">User</a>
-                </li>
-            </ul>
+                <li class="nav-item">
+                        <a class="nav-link inactive" aria-current="page" href="#">LOGIN</a>
+                 </li>
+             </ul>
+        </div>
+        </div>
         </nav>
-       ) 
-
+       )
 }
 
 export default Nav;
